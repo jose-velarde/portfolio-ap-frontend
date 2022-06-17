5322e64ba3af83fe3e34b0456b030d339f905963
Jose Velarde <josevelarde00@gmail.com>
2022-06-17 14:36:28 -0300

    Added profile experience section

---
+++ src/app/profile-experience-education/profile-experience-education.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileExperienceEducationComponent } from './profile-experience-education.component';
+
+describe('ProfileExperienceEducationComponent', () => {
+  let component: ProfileExperienceEducationComponent;
+  let fixture: ComponentFixture<ProfileExperienceEducationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfileExperienceEducationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileExperienceEducationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/profile-experience-education/profile-experience-education.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core'
+import { Experience } from '../interfaces/experience'
+import { Education } from '../interfaces/education'
+import { ProfileService } from '../profile.service'
+
+@Component({
+  selector: 'app-profile-experience-education',
+  templateUrl: './profile-experience-education.component.html',
+  styleUrls: ['./profile-experience-education.component.css'],
+})
+export class ProfileExperienceEducationComponent implements OnInit {
+  constructor(private profileService: ProfileService) {}
+  experiences: Experience[] = []
+  educations: Education[] = []
+
+  showExperiences() {
+    this.profileService.getExperiences().subscribe({
+      next: (experiences) => {
+        this.experiences = experiences
+      },
+    })
+  }
+  showEducations() {
+    this.profileService.getEducations().subscribe({
+      next: (educations) => {
+        this.educations = educations
+      },
+    })
+  }
+
+  ngOnInit(): void {
+    this.showExperiences()
+    this.showEducations()
+  }
+}
